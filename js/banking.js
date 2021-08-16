75c18dc07eac14a114f1f0fdaf0f9f3ef0a68574
zubairhmd84m <zubairhmd84@gmail.com>
2021-08-16 21:17:36 +0600

    added  full function

--- js/banking.js
+++ js/banking.js
@@ -8,6 +8,7 @@ function getInputValue (inputID){
 }
 
 function updateTotalField(totalFieldId, depositAmount){
+    // debugger
     const depositTotal = document.getElementById(totalFieldId);
     // because 'deosit-total' span tag er moddhe use kora hoyse, tai inner text use korsi. ta na hole VALUE use hoto.
     const depositTotalText = depositTotal.innerText;
@@ -15,25 +16,45 @@ function updateTotalField(totalFieldId, depositAmount){
     depositTotal.innerText = previousDepositTotal + depositAmount;
 }
 
-document.getElementById('deposit-button').addEventListener('click', function(){
-    const depositAmount = getInputValue('deposit-input');
-    updateTotalField('deposit-total', depositAmount );
-
-    // update balance
+function getCurrentBalance(){
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
-    balanceTotal.innerText = previousBalanceTotal + depositAmount; 
+    return previousBalanceTotal; 
+}
+
+function updateBanalce(depositAmount, isAdd){
+    const balanceTotal = document.getElementById('balance-total');
+    /* 
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText); 
+    */
+    const previousBalanceTotal =  getCurrentBalance();
+    if(isAdd == true){
+        balanceTotal.innerText = previousBalanceTotal + depositAmount;
+    }
+    else {
+        balanceTotal.innerText = previousBalanceTotal - depositAmount; 
+    }
+}
+
+document.getElementById('deposit-button').addEventListener('click', function(){
+    const depositAmount = getInputValue('deposit-input');
+    if (depositAmount > 0){
+        updateTotalField('deposit-total', depositAmount );
+        updateBanalce(depositAmount, true);
+    }
 });
 
 //handle withdraw
 document.getElementById('withdraw-button').addEventListener('click', function(){
     const withdrawAmount = getInputValue('withdraw-input');
-    updateTotalField('withdraw-total', withdrawAmount);
-
-    // update balance after withdraw
-    const balanceTotal = document.getElementById('balance-total');
-    const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(balanceTotalText);
-    balanceTotal.innerText = previousBalanceTotal - withdrawAmount;    
+    const currentBalance = getCurrentBalance();
+    if (withdrawAmount > 0 && withdrawAmount < currentBalance) {
+        updateTotalField('withdraw-total', withdrawAmount); 
+        updateBanalce(withdrawAmount, false);
+    }
+    if (withdrawAmount > currentBalance){
+        console.log('You have not enough balance to withdraw');
+    }
 })
